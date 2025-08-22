d29850f155d37b29f922ec42d58d63fafdfe58fc
Nandana Narayan <amegha@example.com>
2025-08-22 15:25:32 +0530

    sll

--- DS/sll.c
+++ DS/sll.c
@@ -29,6 +29,37 @@ void insertAtEnd(int v){
     }
     ptr->next=new;
 }
+void insertAtMiddle(int v,int p){
+    struct node* new=createNode(v);
+    struct node *ptr=head;
+    for(int i=1;i<p-1;i++){
+        ptr=ptr->next;
+    }
+    new->next=ptr->next;
+    ptr->next=new;
+}
+void deleteAtBeginning(){
+    struct node *ptr=head;
+    printf("Deleted element: %d\n",ptr->data);
+    head=ptr->next;
+}
+void deleteAtEnd(){
+    struct node *ptr=head;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    printf("Deleted element: %d\n",ptr->data);
+    ptr->next=NULL;
+}
+void deleteAtMiddle(int p){
+    struct node *ptr=head;
+    struct node *temp=head;
+    for(int i=0;i<p-1;i++){
+        ptr=ptr->next;
+        temp=temp->next;
+    }
+    ptr->next=temp->next->next;
+}
 void display(){
     struct node *ptr=head;
     while(ptr!=NULL){
@@ -41,5 +72,11 @@ int main()
     insertAtBeginning(10);
     insertAtBeginning(20);
     insertAtEnd(39);
+    insertAtMiddle(78,2);
     display();
+    //deleteAtBeginning();
+    deleteAtEnd();
+    deleteAtMiddle(2);
+    display();
+    return 0;
 }
