d17e30dc84cb9aaa39e8e2f3abad2e5e64cab0e6
Nandana Narayan <amegha@example.com>
2025-11-07 10:07:14 +0530

    bfs

--- DS/bfs.c
+++ DS/bfs.c
@@ -1,38 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
-void enqueue(int v,int q[],int f,int r){
-    if(f==r==-1){
-        f=0;
-        r=0;
-        q[r]=v;
-    }
-    else{
+int q[10];
+int f=0,r=-1;
+int vis[10]={0};
+void enqueue(int v){
         r++;
         q[r]=v;
+}
+int dequeue(){
+        int a=q[f];
+        f++;
+        return a;
+}
+void bfs(int v,int s,int g[6][6]){
+    enqueue(s);
+    vis[s]=1;
+    printf("BFS Traversal: \n");
+    while(f<=r){
+        int a=dequeue();
+        printf("%d\t",a);
+        for(int i=1;i<=v;i++){
+            if(g[a][i]==1 && !vis[i]){
+                enqueue(i);
+                vis[i]=1;
+            }
+        }
     }
-}
-void dequeue(int q[],int f,int r){
-
-}
-void bfs(int v,int s,int g[5][5]){
-    #define max=10;
-    int q[max];
-    int vis[max];
-    int f=-1;
-    int r=-1;
-    enqueue(s,q,f,r);
-    vis[0]=s;
 }
 int main(){
     int v,s;
     printf("enter number of vertices: ");
     scanf("%d",&v);
-    int g[5][5];
+    int g[6][6];
     printf("enter starting vertex: ");
     scanf("%d",&s);
     printf("enter adjacency matrix: ");
-    for(int i=0;i<v;i++){
-        for(int j=0;j<v;j++){
+    for(int i=1;i<=v;i++){
+        for(int j=1;j<=v;j++){
             printf("v[%d][%d]: ",i,j);
             scanf("%d",&g[i][j]);
         }
