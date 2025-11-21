2f23f2d8b20277c7de298e618da5aaf0922a8d36
Nandana Narayan <amegha@example.com>
2025-11-21 15:38:26 +0530

    kruskals

--- DS/kruskals.c
+++ DS/kruskals.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<limits.h>
 #define MAX 100
-int graph[MAX][MAX],result[MAX][MAX],i,j,u,v,n,min=INT_MAX,cost=0,e=0,parent[MAX];
+int i,j,u,v,n,min,graph[MAX][MAX],parent[MAX];
 int find(int x, int parent[]){
     if(parent[x]==x){
         return x;
@@ -22,6 +22,11 @@ int Union(int x,int y,int parent[]){
     }
 }
 void kruskal(){
+    int cost=0,e=0,u=-1,v=-1;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("a[%d][%d]: ",i,j);
+            scanf("%d",&graph[i][j]);}}
     for(i=0;i<n;i++){
         parent[i]=i;
     }
@@ -29,15 +34,15 @@ void kruskal(){
         min=INT_MAX;
         for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++){
-                if(graph[i][j]<min&&graph[i][j]!=0){
+            for(j=i+1;j<n;j++){
+                if(graph[i][j]<min && graph[i][j]!=0){
                     min=graph[i][j];
                     u=i;
                     v=j;
                 }
             }
         }
-        if(min==INT_MAX){
+        if(u==-1){
             printf("graph is disconnected");
             break;
         }
@@ -45,11 +50,11 @@ void kruskal(){
             graph[v][u]=INT_MAX;
             if(Union(u,v,parent))
             {
-                result[u][v]=min;
-                result[v][u]=min;
+                // result[u][v]=min;
+                // result[v][u]=min;
+            printf("{%d,%d}->%d\n",u,v,min);
                 cost+=min;
                 e++;
-            printf("{%d,%d}->%d\n",u,v,min);
 
             }
     }
@@ -60,11 +65,5 @@ void kruskal(){
 int main(){
     printf("enter the number of vertices:");
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("a[%d][%d]: ",i,j);
-            scanf("%d",&graph[i][j]);
-        }
-    }
     kruskal();
 }
